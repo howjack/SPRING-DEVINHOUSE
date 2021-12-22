c584f85407ad16c9528b2b9e0186d40e52a35e35
Phelipe Alves <phelipe44@gmail.com>
2021-12-22 11:02:30 -0300

    Criei projeto Spring boot com MVC, Security, JDBC e Postgres driver

---
+++ VilaAcate/src/main/java/com/devinhouse/vilacate/VilaAcateApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse.vilacate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilaAcateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VilaAcateApplication.class, args);
+    }
+
+}
---
+++ VilaAcate/src/test/java/com/devinhouse/vilacate/VilaAcateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.vilacate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilaAcateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
